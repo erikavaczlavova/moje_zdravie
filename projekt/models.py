643f66279b7725879bc281ef89e37ba1e5f8093b
tzahradnicek <t.zahradnicek@gmail.com>
2022-03-16 20:08:39 +0100

    First table

--- projekt/models.py
+++ projekt/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
 # class Question(models.Model):
 #     question_text = models.CharField(max_length=200)
 #     pub_date = models.DateTimeField('date published')
 #
 #     def __str__(self):
 #         return self.question_text
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    birthnum = models.CharField(max_length=200)
+    password = models.CharField(max_length=20)
+    birthdate = models.DateTimeField('date of birth')
+    weight = models.IntegerField()
+    height = models.IntegerField()
